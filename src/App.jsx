c19be2c58af1645325ac9418efeaeba221173408
rakeshmiriyala <rakeshmiriyala74@gmail.com>
2025-02-13 14:53:04 +0530

    twentyfive

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 800);
+    }, 4500);
   }, []);
 
   return (
--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,36 +1,15 @@
 import React from "react";
-import { FaCog } from "react-icons/fa";
+import loaderVideo from "../assets/loader_animation.mp4"; // Adjust path based on location
 
 const Loader = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-      <div className="relative">
-        <FaCog className="text-8xl text-[#00a69c] animate-spin" />
-        <FaCog className="text-4xl text-black animate-spin absolute top-[-20px] left-[-20px]" />
-      </div>
+      <video autoPlay loop muted className="w-full">
+        <source src={loaderVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
 
 export default Loader;
-
-
-
-// import React from "react";
-// import logo from "../assets/Logo-1.png"; 
-
-// const Loader = () => {
-//   return (
-//     <div className="flex justify-center items-center h-screen bg-gray-100">
-//       {/* Flipping Logo */}
-//       <img
-//         src={logo}
-//         alt="Loading..."
-//         className="w-24 h-24 animate-flip"
-//       />
-//     </div>
-//   );
-// };
-
-// export default Loader;
-
